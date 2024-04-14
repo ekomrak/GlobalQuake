1079d30ee7e16f4bc9bfa8ebffff1eb1be5d5e40
Evgenii Komrakov <evgenii.komrakov@netcracker.com>
2024-04-14 23:34:46 +0500

    Telegram Bots added

---
+++ GlobalQuakeUserBot/src/main/java/globalquake/telegram/abilities/EarthquakesStatisticAbility.java
@@ -0,0 +1,9 @@
+package globalquake.telegram.abilities;
+
+import globalquake.telegram.TelegramService;
+
+public class EarthquakesStatisticAbility extends AbstractAbility {
+    public EarthquakesStatisticAbility(TelegramService telegramService) {
+        super(telegramService);
+    }
+}
