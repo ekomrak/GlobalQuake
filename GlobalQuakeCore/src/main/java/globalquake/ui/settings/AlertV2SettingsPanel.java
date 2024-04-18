897fe0977253206f496f56bd82ee5b030c1582e3
Evgenii Komrakov <evgenii.komrakov@netcracker.com>
2024-04-19 00:48:25 +0500

    Telegram Bots added

--- GlobalQuakeCore/src/main/java/globalquake/ui/settings/AlertV2SettingsPanel.java
+++ GlobalQuakeCore/src/main/java/globalquake/ui/settings/AlertV2SettingsPanel.java
@@ -1,6 +1,7 @@
 package globalquake.ui.settings;
 
 import globalquake.core.Settings;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,6 +15,8 @@ public class AlertV2SettingsPanel extends SettingsPanel {
     private JCheckBox enableLimitedEarthquakes;
     private JCheckBox enableLimitedArchivedEarthquakes;
     private JTextField stationsLoadDist;
+    private JCheckBox sendImageAsAPhoto;
+    private JCheckBox sendMapAsAPhoto;
 
     private JTextField telegramBotToken;
     private JTextField telegramBotUsername;
@@ -71,17 +74,23 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         generalPanel.setBorder(BorderFactory.createTitledBorder("General Settings"));
 
         enableLimitedStations = new JCheckBox("Limit stations loading", Settings.enableLimitedStations);
-        generalPanel.add(enableLimitedStations, createGbcOneLine(0));
+        generalPanel.add(enableLimitedStations, createGbc(0));
 
         generalPanel.add(new JLabel("Stations load max distance: ", SwingConstants.LEFT), createGbc(0, 1));
         stationsLoadDist = new JTextField(String.format("%s", Settings.stationsLoadDist));
         generalPanel.add(stationsLoadDist, createGbc(1, 1));
 
         enableLimitedEarthquakes = new JCheckBox("Limit earthquake box display", Settings.enableLimitedEarthquakes);
-        generalPanel.add(enableLimitedEarthquakes, createGbcOneLine(2));
+        generalPanel.add(enableLimitedEarthquakes, createGbc(2));
 
         enableLimitedArchivedEarthquakes = new JCheckBox("Limit earthquake archive panel", Settings.enableLimitedArchivedEarthquakes);
-        generalPanel.add(enableLimitedArchivedEarthquakes, createGbcOneLine(3));
+        generalPanel.add(enableLimitedArchivedEarthquakes, createGbc(3));
+
+        sendImageAsAPhoto = new JCheckBox("Send image as a photo", Settings.sendImageAsAPhoto);
+        generalPanel.add(sendImageAsAPhoto, createGbc(4));
+
+        sendMapAsAPhoto = new JCheckBox("Send map as a photo", Settings.sendMapAsAPhoto);
+        generalPanel.add(sendMapAsAPhoto, createGbc(5));
 
         add(generalPanel);
     }
@@ -102,7 +111,7 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         warning.setLineWrap(true);
         warning.setEditable(false);
         warning.setBackground(telegramBotPanel.getBackground());
-        telegramBotPanel.add(warning, createGbcOneLine(2));
+        telegramBotPanel.add(warning, createGbc(2));
 
         telegramBotPanel.add(new JLabel("Telegram Chat Id: ", SwingConstants.LEFT), createGbc(0, 3));
         telegramChatId = new JTextField(String.format("%s", Settings.telegramChatId));
@@ -124,22 +133,22 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         tsEarthquakeAlertPanel.setBorder(BorderFactory.createTitledBorder("Earthquake Alert Settings"));
 
         enableTelegramEarthquakeAlert = new JCheckBox("Enable earthquake alert by Telegram", Settings.enableTelegramEarthquakeAlert);
-        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeAlert, createGbcOneLine(0));
+        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeAlert, createGbc(0));
 
         enableSpeechEarthquakeAlert = new JCheckBox("Enable earthquake alert by Speech", Settings.enableSpeechEarthquakeAlert);
-        tsEarthquakeAlertPanel.add(enableSpeechEarthquakeAlert, createGbcOneLine(1));
+        tsEarthquakeAlertPanel.add(enableSpeechEarthquakeAlert, createGbc(1));
 
         /*enableSoundEarthquakeAlert = new JCheckBox("Enable earthquake alert by Sound", Settings.enableSoundEarthquakeAlert);
-        tsEarthquakeAlertPanel.add(enableSoundEarthquakeAlert, createGbcOneLine(2));*/
+        tsEarthquakeAlertPanel.add(enableSoundEarthquakeAlert, createGbc(2));*/
 
         enableTelegramEarthquakeLocation = new JCheckBox("Send earthquake location", Settings.enableTelegramEarthquakeLocation);
-        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeLocation, createGbcOneLine(2));
+        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeLocation, createGbc(2));
 
         enableTelegramEarthquakeImage = new JCheckBox("Send earthquake image", Settings.enableTelegramEarthquakeImage);
-        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeImage, createGbcOneLine(3));
+        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeImage, createGbc(3));
 
         enableTelegramEarthquakeMap = new JCheckBox("Send earthquake map", Settings.enableTelegramEarthquakeMap);
-        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeMap, createGbcOneLine(4));
+        tsEarthquakeAlertPanel.add(enableTelegramEarthquakeMap, createGbc(4));
 
         JPanel area1Panel = new JPanel(new GridBagLayout());
         area1Panel.setBorder(BorderFactory.createTitledBorder("Area 1"));
@@ -152,7 +161,7 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         tsEarthquakeMaxDistArea1 = new JTextField(String.format("%s", Settings.tsEarthquakeMaxDistArea1));
         area1Panel.add(tsEarthquakeMaxDistArea1, createGbc(1, 1));
 
-        tsEarthquakeAlertPanel.add(area1Panel, createGbcOneLine(5));
+        tsEarthquakeAlertPanel.add(area1Panel, createGbc(5));
 
         JPanel area2Panel = new JPanel(new GridBagLayout());
         area2Panel.setBorder(BorderFactory.createTitledBorder("Area 2"));
@@ -165,7 +174,7 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         tsEarthquakeMaxDistArea2 = new JTextField(String.format("%s", Settings.tsEarthquakeMaxDistArea2));
         area2Panel.add(tsEarthquakeMaxDistArea2, createGbc(1, 1));
 
-        tsEarthquakeAlertPanel.add(area2Panel, createGbcOneLine(6));
+        tsEarthquakeAlertPanel.add(area2Panel, createGbc(6));
 
         tsEarthquakeMinIntensity = new IntensityScaleSelector("Earthquake alert min intensity:", Settings.tsEarthquakeIntensityScale, Settings.tsEarthquakeMinIntensity);
         tsEarthquakeAlertPanel.add(tsEarthquakeMinIntensity, createGbc(1, 7));
@@ -178,22 +187,22 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         tsPossibleShakingAlertPanel.setBorder(BorderFactory.createTitledBorder("Possible Shaking Alert Settings"));
 
         enableTelegramPossibleShakingAlert = new JCheckBox("Enable possible shaking alert by Telegram", Settings.enableTelegramPossibleShakingAlert);
-        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingAlert, createGbcOneLine(0));
+        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingAlert, createGbc(0));
 
         enableSpeechPossibleShakingAlert = new JCheckBox("Enable possible shaking alert by Speech", Settings.enableSpeechPossibleShakingAlert);
-        tsPossibleShakingAlertPanel.add(enableSpeechPossibleShakingAlert, createGbcOneLine(1));
+        tsPossibleShakingAlertPanel.add(enableSpeechPossibleShakingAlert, createGbc(1));
 
         /*enableSoundPossibleShakingAlert = new JCheckBox("Enable possible shaking alert by Sound", Settings.enableSoundPossibleShakingAlert);
-        tsPossibleShakingAlertPanel.add(enableSoundPossibleShakingAlert, createGbcOneLine(2));*/
+        tsPossibleShakingAlertPanel.add(enableSoundPossibleShakingAlert, createGbc(2));*/
 
         enableTelegramPossibleShakingLocation = new JCheckBox("Send earthquake location", Settings.enableTelegramPossibleShakingLocation);
-        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingLocation, createGbcOneLine(2));
+        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingLocation, createGbc(2));
 
         enableTelegramPossibleShakingImage = new JCheckBox("Send earthquake image", Settings.enableTelegramPossibleShakingImage);
-        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingImage, createGbcOneLine(3));
+        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingImage, createGbc(3));
 
         enableTelegramPossibleShakingMap = new JCheckBox("Send earthquake map", Settings.enableTelegramPossibleShakingMap);
-        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingMap, createGbcOneLine(4));
+        tsPossibleShakingAlertPanel.add(enableTelegramPossibleShakingMap, createGbc(4));
 
         tsPossibleShakingAlertPanel.add(new JLabel("Possible Shaking alert min level: ", SwingConstants.LEFT), createGbc(0, 5));
         tsPossibleShakingMinLevel = new JTextField(String.format("%s", Settings.tsPossibleShakingMinLevel));
@@ -212,22 +221,22 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         tsStationHighIntensityAlertPanel.setBorder(BorderFactory.createTitledBorder("Station High Intensity Alert Settings"));
 
         enableTelegramStationHighIntensityAlert = new JCheckBox("Enable station high intensity alert by Telegram", Settings.enableTelegramStationHighIntensityAlert);
-        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityAlert, createGbcOneLine(0));
+        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityAlert, createGbc(0));
 
         enableSpeechStationHighIntensityAlert = new JCheckBox("Enable station high intensity alert by Speech", Settings.enableSpeechStationHighIntensityAlert);
-        tsStationHighIntensityAlertPanel.add(enableSpeechStationHighIntensityAlert, createGbcOneLine(1));
+        tsStationHighIntensityAlertPanel.add(enableSpeechStationHighIntensityAlert, createGbc(1));
 
         /*enableSoundStationHighIntensityAlert = new JCheckBox("Enable station high intensity alert by Sound", Settings.enableSoundStationHighIntensityAlert);
-        tsStationHighIntensityAlertPanel.add(enableSoundStationHighIntensityAlert, createGbcOneLine(2));*/
+        tsStationHighIntensityAlertPanel.add(enableSoundStationHighIntensityAlert, createGbc(2));*/
 
         enableTelegramStationHighIntensityLocation = new JCheckBox("Send earthquake location", Settings.enableTelegramStationHighIntensityLocation);
-        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityLocation, createGbcOneLine(2));
+        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityLocation, createGbc(2));
 
         enableTelegramStationHighIntensityImage = new JCheckBox("Send earthquake image", Settings.enableTelegramStationHighIntensityImage);
-        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityImage, createGbcOneLine(3));
+        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityImage, createGbc(3));
 
         enableTelegramStationHighIntensityMap = new JCheckBox("Send earthquake map", Settings.enableTelegramStationHighIntensityMap);
-        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityMap, createGbcOneLine(4));
+        tsStationHighIntensityAlertPanel.add(enableTelegramStationHighIntensityMap, createGbc(4));
 
         JPanel area1Panel = new JPanel(new GridBagLayout());
         area1Panel.setBorder(BorderFactory.createTitledBorder("Area 1"));
@@ -240,7 +249,7 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         tsStationMaxDist1 = new JTextField(String.format("%s", Settings.tsStationMaxDist1));
         area1Panel.add(tsStationMaxDist1, createGbc(1, 1));
 
-        tsStationHighIntensityAlertPanel.add(area1Panel, createGbcOneLine(5));
+        tsStationHighIntensityAlertPanel.add(area1Panel, createGbc(5));
 
         JPanel area2Panel = new JPanel(new GridBagLayout());
         area2Panel.setBorder(BorderFactory.createTitledBorder("Area 2"));
@@ -253,7 +262,7 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         tsStationMaxDist2 = new JTextField(String.format("%s", Settings.tsStationMaxDist2));
         area2Panel.add(tsStationMaxDist2, createGbc(1, 1));
 
-        tsStationHighIntensityAlertPanel.add(area2Panel, createGbcOneLine(6));
+        tsStationHighIntensityAlertPanel.add(area2Panel, createGbc(6));
 
         add(tsStationHighIntensityAlertPanel);
     }
@@ -263,7 +272,7 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         speechServiceAlertPanel.setBorder(BorderFactory.createTitledBorder("Speech Service Settings"));
 
         useFreeTts = new JCheckBox("Use FreeTTS (free) instead of Google Text-to-Speech (paid)", Settings.useFreeTts);
-        speechServiceAlertPanel.add(useFreeTts, createGbcOneLine(0));
+        speechServiceAlertPanel.add(useFreeTts, createGbc(0));
 
         add(speechServiceAlertPanel);
     }
@@ -274,6 +283,8 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         Settings.enableLimitedStations = enableLimitedStations.isSelected();
         Settings.enableLimitedEarthquakes = enableLimitedEarthquakes.isSelected();
         Settings.enableLimitedArchivedEarthquakes = enableLimitedArchivedEarthquakes.isSelected();
+        Settings.sendImageAsAPhoto = sendImageAsAPhoto.isSelected();
+        Settings.sendMapAsAPhoto = sendMapAsAPhoto.isSelected();
 
         Settings.telegramBotToken = telegramBotToken.getText();
         Settings.telegramBotUsername = telegramBotUsername.getText();
@@ -322,11 +333,19 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         return "Alert V2";
     }
 
-    private GridBagConstraints createGbc(int x, int y) {
+    protected static GridBagConstraints createGbc(int y) {
+        return createGbc(0, y, 2);
+    }
+
+    protected static GridBagConstraints createGbc(int x, int y) {
+        return createGbc(x, y, 1);
+    }
+
+    protected static GridBagConstraints createGbc(int x, int y, int gridwidth) {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = x;
         gbc.gridy = y;
-        gbc.gridwidth = 1;
+        gbc.gridwidth = gridwidth;
         gbc.gridheight = 1;
 
         gbc.anchor = (x == 0) ? GridBagConstraints.WEST : GridBagConstraints.EAST;
@@ -339,19 +358,74 @@ public class AlertV2SettingsPanel extends SettingsPanel {
         return gbc;
     }
 
-    private GridBagConstraints createGbcOneLine(int y) {
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = y;
-        gbc.gridwidth = 2;
-        gbc.gridheight = 1;
+    protected static Component createJTextArea(String text, Component parent) {
+        JTextArea jTextArea = new JTextArea(text);
+        jTextArea.setLineWrap(true);
+        jTextArea.setEditable(false);
+        jTextArea.setBackground(parent.getBackground());
 
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.fill = GridBagConstraints.BOTH;
+        return jTextArea;
+    }
 
-        gbc.insets = WEST_INSETS;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-        return gbc;
+    protected static JPanel createGridBagPanel() {
+        return createGridBagPanel(true);
+    }
+
+    protected static JPanel createGridBagPanel(boolean emptyBorder) {
+        return createGridBagPanel(null, emptyBorder);
+    }
+
+    protected static JPanel createGridBagPanel(String borderText) {
+        return createGridBagPanel(borderText, false);
+    }
+
+    protected static JPanel createGridBagPanel(String borderText, boolean emptyBorder) {
+        JPanel panel = new JPanel(new GridBagLayout());
+        if (StringUtils.isEmpty(borderText)) {
+            if (emptyBorder) {
+                panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            }
+        } else {
+            panel.setBorder(BorderFactory.createTitledBorder(borderText));
+        }
+        return panel;
+    }
+
+    protected static JPanel createVerticalPanel() {
+        return createVerticalPanel(true);
+    }
+
+    protected static JPanel createVerticalPanel(boolean emptyBorder) {
+        return createVerticalPanel(null, emptyBorder);
+    }
+
+    protected static JPanel createVerticalPanel(String borderText) {
+        return createVerticalPanel(borderText, false);
+    }
+
+    protected static JPanel createVerticalPanel(String borderText, boolean emptyBorder) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        if (StringUtils.isEmpty(borderText)) {
+            if (emptyBorder) {
+                panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            }
+        } else {
+            panel.setBorder(BorderFactory.createTitledBorder(borderText));
+        }
+        return  panel;
+    }
+
+    protected static JPanel createHorizontalPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        return panel;
+    }
+
+    protected static Component alignLeft(Component component) {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(component, BorderLayout.WEST);
+        return panel;
     }
 }
+
