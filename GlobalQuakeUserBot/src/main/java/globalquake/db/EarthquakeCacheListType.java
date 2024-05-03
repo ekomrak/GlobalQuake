ddb33bc2e8a26eef07e9415c2b426364afac0de6
Evgenii Komrakov <evgenii.komrakov@netcracker.com>
2024-05-03 18:02:10 +0500

    Refactoring

--- GlobalQuakeUserBot/src/main/java/globalquake/db/EarthquakeCacheListType.java
+++ GlobalQuakeUserBot/src/main/java/globalquake/db/EarthquakeCacheListType.java
@@ -1,5 +1,24 @@
 package globalquake.db;
 
 public enum EarthquakeCacheListType {
-    ALL
+    ALL("all");
+
+    private final String name;
+
+    EarthquakeCacheListType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static EarthquakeCacheListType findByName(String name){
+        for(EarthquakeCacheListType usersCacheListType : values()){
+            if(usersCacheListType.getName().equalsIgnoreCase(name)){
+                return usersCacheListType;
+            }
+        }
+        return null;
+    }
 }
